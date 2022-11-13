85db2e2c1891e2c17adc2621ee6fc5cb2dba0125
MurylloEx <muryllopimenta@gmail.com>
2022-11-13 19:55:55 -0300

    feat: installed and configured Jest for unit tests

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  silent: true,
+  preset: 'ts-jest',
+  moduleDirectories: ['src', 'node_modules'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts'
+  ],
+  rootDir: './',
+  modulePaths: ['<rootDir>'],
+  testRegex: '(.*\\.test\\.ts)$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s'
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node'
+};
+
+export default config;
